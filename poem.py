56a4d6d04b9840a0d320e1a4812c7428894698ac
Maciej <rybczynskimaciek@gmail.com>
2025-10-14 23:15:20 +0200

    Scripts 14.10

---
+++ poem.py
@@ -0,0 +1,27 @@
+# %%poem = '''1. Runą w łunach, spłoną w pożarach
+Krzyże kościołów, krzyże ofiarne
+I w bezpowrotnym zgubi się szlaku
+Z Lechickiej ziemi Orzeł Polaków
+
+2. O słońce jasne, wodzu Stalinie
+Niech władza twoja nigdy nie zginie
+Niech jako orłów prowadzi z gniazda
+Rosji i Kremla płonąca gwiazda
+
+3. Na ziemskim globie flagi czerwone
+Będą na chwałę grały jak dzwony
+Czerwona Armia i wódz jej Stalin
+Odwiecznych wrogów swoich obali
+
+4. Zmienisz się rychło w wieku godzinie
+Polsko, a twoje córy i syny
+Wiara i każdy krzyż na mogile
+U stóp nam legnie w prochu i pyle'''
+
+lines = poem.split('\n')
+i = 0
+
+for i in range(8):
+        print(lines[i])
+        print(lines[i+8])
+# %%
---
+++ time_calendar.py
@@ -0,0 +1,9 @@
+# %%import time
+import sys
+print(sys.version)
+
+#print(time.clock(), time.localtime(time.clock()))
+print(time.perf_counter(), time.localtime(time.perf_counter()))
+
+
+# %%
